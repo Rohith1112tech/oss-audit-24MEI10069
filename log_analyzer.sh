085d5873df896ed5fd1bbeea18856150516b336d
ZORO <rohithrajrs63@gmail.com>
2026-03-31 00:09:50 +0530

    Create log_analyzer.sh

---
+++ log_analyzer.sh
@@ -0,0 +1,11 @@
+%%bash
+LOGFILE="test.log"
+KEYWORD="error"
+COUNT=0
+echo "Analyzing file: $LOGFILE"
+while IFS= read -r LINE; do
+    if echo "$LINE" | grep -iq "$KEYWORD"; then
+        COUNT=$((COUNT + 1))
+    fi
+done < "$LOGFILE"
+echo "Keyword '$KEYWORD' found $COUNT times."
